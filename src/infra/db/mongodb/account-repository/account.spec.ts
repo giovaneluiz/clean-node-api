43c794fb9138e29a74982a48d66f4b626e9a1911
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-01-24 15:54:05 -0300

    refactor: move mongdb methods to a helper

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+})
