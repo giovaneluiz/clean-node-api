965c74f3796559599edb39f42bfe7c8e74c455c2
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-01-24 14:56:22 -0300

    test: ensure Bcrypt adapter throw if bcrypt throws

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -1,4 +1,4 @@
-import bcrypt, { hash } from 'bcrypt'
+import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
@@ -6,7 +6,6 @@ jest.mock('bcrypt', () => ({
     return new Promise(resolve => resolve('hash'))
   }
 }))
-
 const salt = 12
 
 const makeSut = (): BcryptAdapter => {
@@ -14,16 +13,25 @@ const makeSut = (): BcryptAdapter => {
 }
 
 describe('Bcrypt Adapter', () => {
-  test('should call bcrypt with correct values', async () => {
-    const sut = makeSut()
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
-  })
+  describe('hash()', () => {
+    test('Should call hash with correct value', async () => {
+      const sut = makeSut()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.encrypt('any_value')
+      expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
 
-  test('should return a hash on success', async () => {
-    const sut = makeSut()
-    const hash = await sut.encrypt('any_value')
-    expect(hash).toBe('hash')
+    test('Should return a valida hash on hash succes', async () => {
+      const sut = makeSut()
+      const hash = await sut.encrypt('any_value')
+      expect(hash).toBe('hash')
+    })
+
+    test('Should throw if bcrypter throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.encrypt('any_value')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
