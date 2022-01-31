14b82e943df9048e0b6a28a8b6a55beabfa13263
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-01-30 21:41:47 -0300

    refactor: change Authentication to a receive a model

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,3 +1,8 @@
+export interface AuthenticationParams {
+  email: string
+  password: string
+}
+
 export interface Authentication {
-  auth: (email: string, password: string) => Promise<string>
+  auth: (authentication: AuthenticationParams) => Promise<string>
 }
