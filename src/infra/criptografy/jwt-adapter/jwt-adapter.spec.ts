3b377f5dfe85dd194a754510042720a18d2101bb
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-01-31 14:27:08 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptografy/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,11 @@
+import { JwtAdapter } from './jwt-adapter'
+import jwt from 'jsonwebtoken'
+
+describe('JWT Adapter', () => {
+  test('Should call sign with correct values', async () => {
+    const sut = new JwtAdapter('secret')
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenLastCalledWith({ id: 'any_id' }, 'secret')
+  })
+})
---
+++ src/infra/criptografy/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,11 @@
+import { Encrypter } from '../../../data/protocols/criptography/encrypter'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements Encrypter {
+  constructor (private readonly secret: string) {}
+
+  async encrypt (value: string): Promise<string> {
+    await jwt.sign({ id: value }, this.secret)
+    return null
+  }
+}
