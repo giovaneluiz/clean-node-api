363001585a39c277bddc85d1fd939412ac261981
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-01-31 11:49:08 -0300

    test: ensure Bcrypt Adapter return trows if compare throws

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -57,5 +57,12 @@ describe('Bcrypt Adapter', () => {
       const isValid = await sut.compare('any_value', 'any_hash')
       expect(isValid).toBe(false)
     })
+
+    test('Should throw if compare throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
