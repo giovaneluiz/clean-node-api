76e067d66cb6aa021fb10ffd70f9b32cb78be6f8
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-01-30 21:07:08 -0300

    test: ensure validation composite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -44,4 +44,12 @@ describe('Validation Composite', () => {
     })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds ', () => {
+    const { sut, validationStubs } = makeSut()
+    const error = sut.validate({
+      field: 'any_value'
+    })
+    expect(error).toBeFalsy()
+  })
 })
