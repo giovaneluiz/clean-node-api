a33cca2cd230f03fd175127d047483407089ff7d
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-01-31 10:27:50 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -21,8 +21,11 @@ export class DbAuthentication implements Authentication {
   async auth (authentication: AuthenticationParams): Promise<string> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
-      await this.hashComparer.compare(authentication.password, account.password)
-      await this.tokenGenerator.generate(account.id)
+      const isValid = await this.hashComparer.compare(authentication.password, account.password)
+      if (isValid) {
+        const accesToken = await this.tokenGenerator.generate(account.id)
+        return accesToken
+      }
     }
     return null
   }
