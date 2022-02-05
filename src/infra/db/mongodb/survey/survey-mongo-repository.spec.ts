6190150fde4672c79c56e598b1fc180526bcffb6
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-02-05 15:40:33 -0300

    test: ensure SurveyMongoRepository add an a survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,42 @@
+import { SurveyMongoRepository } from './survey-mongo-repository'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import env from '../../../../main/config/env'
+
+let surveyCollection: Collection
+
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should add an a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answer: 'any_answer'
+      }, {
+        answer: 'any_answer'
+      }]
+    })
+    const surveyItem = await surveyCollection.findOne({
+      question: 'any_question'
+    })
+    expect(surveyItem).toBeTruthy()
+  })
+})
