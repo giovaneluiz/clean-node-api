11e5e7b84131cad63c83f07b408185f473ff3c6e
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-02-07 11:27:36 -0300

    test: ensure load surveys route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -39,36 +39,43 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
-  })
 
-  test('Should Return 204 on add survey valid token', async () => {
-    const account = await accountCollection.insertOne({
-      name: 'Giovane',
-      email: 'giovane@mail.com',
-      password: '123',
-      role: 'admin'
-    })
-    const id = account.ops[0]._id
-    const accessToken = sign({ id }, env.jwtSecret)
-    await accountCollection.updateOne({
-      _id: id
-    }, {
-      $set: {
-        accessToken
-      }
-    })
-    await request(app)
-      .post('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Question',
-        answers: [{
-          image: 'http://image-name.com',
-          answer: 'Answer 1'
-        }, {
-          answer: 'Answer 2'
-        }]
+    test('Should Return 204 on add survey valid token', async () => {
+      const account = await accountCollection.insertOne({
+        name: 'Giovane',
+        email: 'giovane@mail.com',
+        password: '123',
+        role: 'admin'
       })
-      .expect(204)
+      const id = account.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            image: 'http://image-name.com',
+            answer: 'Answer 1'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+  describe('GET /surveys', () => {
+    test('Should Return 403 on load surveys without accessToken', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .expect(403)
+    })
   })
 })
