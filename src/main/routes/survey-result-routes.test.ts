da0c66ef7bf3e5601439dfb1e91b69e38eab73fb
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-02-10 19:32:26 -0300

    test: ensure save survey result routes returns 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,56 @@
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import app from '../config/app'
+import request from 'supertest'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const account = await accountCollection.insertOne({
+    name: 'Giovane',
+    email: 'giovane@mail.com',
+    password: '123',
+    role: 'admin'
+  })
+  const id = account.ops[0]._id
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should Return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
