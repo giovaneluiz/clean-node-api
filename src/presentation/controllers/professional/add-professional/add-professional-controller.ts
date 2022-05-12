f3e20cf82906ebf842e468313ba073fbda671517
giovaneluiz <giovaneluiz19@yahoo.com.br>
2022-05-12 09:29:52 -0300

    feat: ensure AddProfessionalController throw if AddProfessional throws

--- src/presentation/controllers/professional/add-professional/add-professional-controller.ts
+++ src/presentation/controllers/professional/add-professional/add-professional-controller.ts
@@ -1,11 +1,16 @@
 import { AddProfessional } from '@/domain/usecases/professional/add-professional'
+import { serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class AddProfessionalController implements Controller {
   constructor (private readonly addProfessional: AddProfessional) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.addProfessional.add(httpRequest.body)
-    return null
+    try {
+      await this.addProfessional.add(httpRequest.body)
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
