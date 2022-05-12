97b0b0bd77cd265435f1cb3df876e7637d5d5fff
giovaneluiz <giovaneluiz19@yahoo.com.br>
2022-05-12 10:44:23 -0300

    fix: returns professional

--- src/infra/db/mongodb/professional/professional-mongo-repository.ts
+++ src/infra/db/mongodb/professional/professional-mongo-repository.ts
@@ -6,7 +6,7 @@ import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 export class ProfessionalMongoRepository implements AddProfessionalRepository {
   async add (professionalData: AddProfessionalParams): Promise<ProfessionalModel> {
     const professionalCollection = await MongoHelper.getCollection('surveys')
-    const professional = await professionalCollection.insertOne(professionalData)
-    return professional && MongoHelper.map(professional)
+    const result = await professionalCollection.insertOne(professionalData)
+    return MongoHelper.map(result.ops[0])
   }
 }
