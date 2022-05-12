18ab1b1bb71b943b8dbeb4a54d6361db02dedb47
giovaneluiz <giovaneluiz19@yahoo.com.br>
2022-05-12 09:29:07 -0300

    test: ensure AddProfessionalController calls AddProfessional with correct values

---
+++ src/presentation/controllers/professional/add-professional/add-professional-controller.spec.ts
@@ -0,0 +1,43 @@
+import { throwError } from '@/domain/test'
+import { AddProfessional } from '@/domain/usecases/professional/add-professional'
+import { AddProfessionalController } from '@/presentation/controllers/professional/add-professional/add-professional-controller'
+import { serverError } from '@/presentation/helpers/http/http-helper'
+import { HttpRequest } from '@/presentation/protocols'
+import { mockAddProfessional } from '@/presentation/test'
+
+const mockRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    birthDate: 'any_birth_date',
+    indentifier: 12345678900,
+    phone: 99999999999,
+    email: 'any_email@mail.com',
+    percent: 0.6
+  }
+})
+
+type SutTypes = {
+  sut: AddProfessionalController
+  addProfessionalStub: AddProfessional
+
+}
+
+const makeSut = (): SutTypes => {
+  const addProfessionalStub = mockAddProfessional()
+  const sut = new AddProfessionalController(addProfessionalStub)
+
+  return {
+    sut,
+    addProfessionalStub
+  }
+}
+
+describe('AddProfessional Controller', () => {
+  test('Should call AddProfessional with correct values', async () => {
+    const { sut, addProfessionalStub } = makeSut()
+    const addProfessionalSpy = jest.spyOn(addProfessionalStub, 'add')
+    const httpRequest = mockRequest()
+    await sut.handle(httpRequest)
+    expect(addProfessionalSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+})
