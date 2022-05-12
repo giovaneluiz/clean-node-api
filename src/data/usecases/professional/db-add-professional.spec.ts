126fd36f0beb73391ac981bd2afc10a228d1ba6e
giovaneluiz <giovaneluiz19@yahoo.com.br>
2022-05-12 08:11:52 -0300

    test: ebnsure DbAddProfessional throw if AddProfessionalRepository throws

--- src/data/usecases/professional/db-add-professional.spec.ts
+++ src/data/usecases/professional/db-add-professional.spec.ts
@@ -1,6 +1,7 @@
 import { AddProfessionalRepository } from '@/data/protocols/db/professional/add-professional-repository'
 import { mockAddProfessionalRepository } from '@/data/test'
 import { DbAddProfessional } from '@/data/usecases/professional/db-add-professional'
+import { throwError } from '@/domain/test'
 import { mockAddProfessionalParams } from '@/domain/test/mock-professional'
 
 type SutTypes = {
@@ -31,5 +32,12 @@ describe('DbAddProfessional', () => {
       email: 'any_email@mail.com',
       percent: 0.6
     })
+  })
+
+  test('Should throw if AddProfessionalRepository throws', async () => {
+    const { sut, addProfessionalRepositoryStub } = makeSut()
+    jest.spyOn(addProfessionalRepositoryStub, 'add').mockImplementationOnce(throwError)
+    const promise = sut.add(mockAddProfessionalParams())
+    await expect(promise).rejects.toThrow()
   })
 })
